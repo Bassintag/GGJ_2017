5da9fc421a42c2d54cc3db9b60f60c2e8568267b
stessy mougeot <stessy.mougeot@epitech.eu>
2017-01-21 02:19:17 +0100

    sound bumper and setter bpm

--- Assets/MoveObject.cs
+++ Assets/MoveObject.cs
@@ -4,32 +4,51 @@ using UnityEngine;
 
 public class MoveObject : MonoBehaviour {
     private Rigidbody2D _object;
+    private bool _pulse;
+    private Renderer _renderer;
+    private float nextActionTime = 0.0f;
+    public float _period;
+
+
     // Use this for initialization
     void Start () {
 		_object = GetComponent<Rigidbody2D>();
+        _renderer = GetComponent<Renderer>();
+        _renderer.material.color = Color.white;
+        setBpm();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        BumpSound();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void setBpm(float bpm = 140){
+        _period = (float) (60/bpm);
+    }
+
+    void BumpSound()
+    {
+        if (Time.time > (nextActionTime * 2))
+        {
+            if (_renderer.material.color == Color.black)
+                _renderer.material.color = Color.white;
+            else
+                _renderer.material.color = Color.black;
+            nextActionTime += _period;
+        }
+    }
 
     void OnCollisionEnter(Collision c)
     {
-        // force is how forcefully we will push the player away from the enemy.
         float force = 3;
 
-        // If the object we hit is the enemy
         if (c.gameObject.tag == "Player")
         {
-            // Calculate Angle Between the collision point and the player
             Vector3 dir = c.contacts[0].point - transform.position;
-            // We then get the opposite (-Vector3) and normalize it
+
             dir = -dir.normalized;
-            // And finally we add force in the direction of dir and multiply it by force. 
-            // This will push back the player
             _object.velocity = new Vector2(force * dir.x, force * dir.y);
-            //_object.AddForce(dir * force);
         }
     }
 }
