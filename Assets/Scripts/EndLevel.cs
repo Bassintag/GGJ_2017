ea6d7208ea0e77d73dc705175280b070309d96cb
Arthur Melin <arthur.melin@epitech.eu>
2017-01-21 22:27:04 +0100

    Scene loader

--- Assets/Scripts/EndLevel.cs
+++ Assets/Scripts/EndLevel.cs
@@ -5,12 +5,13 @@ using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(BoxCollider2D))]
 public class EndLevel : MonoBehaviour {
+    public SceneLoader lvlLoader;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
-            int lvl = int.Parse(SceneManager.GetActiveScene().name.Remove(0, 6));
-            SceneManager.LoadScene("Level-" + (lvl + 1));
+            lvlLoader.LoadScene();
         }
     }   
 }
