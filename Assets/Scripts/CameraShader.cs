ec165abe55799e98b9c86bd15708139081f85445
Arthur Melin <arthur.melin@epitech.eu>
2017-01-21 14:04:01 +0100

    Invert and chromatic aberration shaders

---
+++ Assets/Scripts/CameraShader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraShader : MonoBehaviour
+{
+    public Shader shader;
+    [Range(0.0f, 1.0f)]
+    public float aberration;
+    private Material mat;
+
+    void Awake()
+    {
+        mat = new Material(shader);
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        mat.SetFloat("_Aberration", aberration);
+        Graphics.Blit(source, destination, mat);
+    }
+}
---
+++ Assets/Shaders/Invert.shader
@@ -0,0 +1,31 @@
+﻿Shader "Hidden/Chroma"
+{
+	Properties
+	{
+		_MainTex ("Tex", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Tags
+		{
+			"Queue"="Transparent"
+		}
+		Pass
+		{
+			CGPROGRAM
+			#pragma fragment frag
+			#pragma vertex vert_img
+
+			#include "UnityCG.cginc"
+
+			sampler2D _MainTex;
+
+			fixed4 frag (v2f_img i) : SV_TARGET
+			{
+				fixed4 col = tex2D(_MainTex, i.uv);
+				return 1-col;
+			}
+			ENDCG
+		}
+	}
+}
