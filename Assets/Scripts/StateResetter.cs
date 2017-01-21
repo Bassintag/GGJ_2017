aa80926f0e14be9c54fb13398dc5696dba8cd5a1
stessy mougeot <stessy.mougeot@epitech.eu>
2017-01-21 18:45:15 +0100

    Good buttons & surround loop on all levels

--- Assets/Scripts/StateResetter.cs
+++ Assets/Scripts/StateResetter.cs
@@ -7,18 +7,22 @@ public class StateResetter : MonoBehaviour {
     private Vector3 _position;
     private Quaternion _rotation;
     private Vector3 _scale;
+    private Collider2D _collider;
 
     void Start ()
     {
         _position = transform.position;
         _rotation = transform.rotation;
         _scale = transform.localScale;
-	}
+        _collider = GetComponent<Collider2D>();
+    }
 	
     public void Reset()
     {
         transform.position = _position;
         transform.rotation = _rotation;
         transform.localScale = _scale;
+        _collider.enabled = false;
+        _collider.enabled = true;
     }
 }
